f16f610f090d250409f69a9ab711f6f1b0162d02
Andrei Gabor <andrei.gabor14@gmail.com>
2023-09-11 20:01:27 +0300

    first commit

---
+++ Idei_retete/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Idei_retete project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Idei_retete.settings')
+
+application = get_wsgi_application()
